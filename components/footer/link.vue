665b5491424907648350a823699c4e9627c1fc65
Maxime Deroullers <maxime.deroullers@gmail.com>
2024-08-17 18:06:21 +0200

    style: update quick links ui

--- components/footer/link.vue
+++ components/footer/link.vue
@@ -1,27 +1,22 @@
+<script setup lang="ts">
+const menuItems = [
+  { label: 'Accueil', to: '/' },
+  { label: 'Tous nos Articles', to: '/blogs' },
+  { label: 'Catégories', to: '/categories' },
+  { label: 'À Propos', to: 'https://hoppr.tech' },
+]
+</script>
+
 <template>
   <div class="flex flex-col dark:text-zinc-300 my-5 md:my-0 md:justify-self-center">
-    <p class="text-black dark:text-zinc-300 text-base font-semibold mb-2">
+    <p class="text-black dark:text-zinc-300 text-base font-semibold mb-4">
       Liens Rapides
     </p>
-    <ul class="list-disc list-inside">
-      <li>
-        <NuxtLink to="/" class="hover:underline">
-          Accueil
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/blogs" class="hover:underline">
-          Tous nos Articles
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/categories" class="hover:underline">
-          Catégories
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="https://hoppr.tech" class="hover:underline">
-          À Propos
+    <ul class="space-y-3">
+      <li v-for="item in menuItems" :key="item.to" class="flex items-center">
+        <span class="text-hoppr-green mr-2">➤</span>
+        <NuxtLink :to="item.to" class="hover:underline py-0.5">
+          {{ item.label }}
         </NuxtLink>
       </li>
     </ul>

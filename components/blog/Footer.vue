06bb2ab9d63a113aaa3d41f00f6aed9b62e31fe2
Maxime Deroullers <maxime.deroullers@gmail.com>
2024-09-27 08:10:00 +0200

    style(author): :lipstick: update single author style

--- components/blog/Footer.vue
+++ components/blog/Footer.vue
@@ -6,20 +6,22 @@ interface Props {
 }
 
 const props = defineProps<Props>()
+
+const isSingleAuthor = computed(() => props.authors.length === 1)
 </script>
 
 <template>
   <div class="py-5 border-t dark:border-zinc-500 mt-5 text-zinc-700 dark:text-zinc-300">
     <div class="container max-w-6xl mx-auto">
       <h3 class="text-xl font-semibold mb-4 text-center">
-        À propos des auteurs
+        {{ isSingleAuthor ? 'À propos de l\'auteur' : 'À propos des auteurs' }}
       </h3>
       <div class="flex flex-col items-center">
-        <div :class="{ 'grid grid-cols-1 sm:grid-cols-2 gap-6 max-w-2xl': props.authors.length > 1, 'flex justify-center': props.authors.length === 1 }">
+        <div :class="{ 'grid grid-cols-1 sm:grid-cols-2 gap-6 max-w-2xl': !isSingleAuthor, 'flex justify-center': isSingleAuthor }">
           <template v-for="(author, index) in props.authors" :key="author.notionId">
-            <div :class="{ 'flex flex-col sm:flex-row items-center mb-4 sm:mb-0 relative sm:px-4': props.authors.length > 1, 'flex flex-col items-center': props.authors.length === 1 }">
+            <div :class="{ 'flex flex-col sm:flex-row items-center mb-4 sm:mb-0 relative sm:px-4': !isSingleAuthor, 'flex flex-row items-center': isSingleAuthor }">
               <img :src="author.image" :alt="author.name" class="w-16 h-16 rounded-full mb-2 sm:mb-0 sm:mr-4 object-cover">
-              <div :class="{ 'text-center sm:text-left': props.authors.length > 1, 'text-center': props.authors.length === 1 }">
+              <div :class="{ 'text-center sm:text-left': !isSingleAuthor, 'text-left ml-4': isSingleAuthor }">
                 <p class="font-semibold text-base">
                   {{ author.name }}
                 </p>
@@ -38,7 +40,7 @@ const props = defineProps<Props>()
                   </a>
                 </div>
               </div>
-              <div v-if="props.authors.length > 1 && index % 2 === 0 && index !== props.authors.length - 1" class="hidden sm:block absolute right-0 top-0 bottom-0 w-px bg-gray-300 dark:bg-zinc-600" />
+              <div v-if="!isSingleAuthor && index % 2 === 0 && index !== props.authors.length - 1" class="hidden sm:block absolute right-0 top-0 bottom-0 w-px bg-gray-300 dark:bg-zinc-600" />
             </div>
           </template>
         </div>
